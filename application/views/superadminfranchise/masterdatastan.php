9706d93ed74d1c4fa8e7d9201f125d91aabd14b8
Ivan Christianto Onggara <36890340+IvanOnggara@users.noreply.github.com>
2018-09-10 17:23:32 +0700

    S

--- application/views/superadminfranchise/masterdatastan.php
+++ application/views/superadminfranchise/masterdatastan.php
@@ -294,22 +294,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <script>
 
   var tabeldata ;
-  ( function ( $ ) {
-            "use strict";
+  // ( function ( $ ) {
+  //           "use strict";
 
-            jQuery( '#vmap' ).vectorMap( {
-                map: 'world_en',
-                backgroundColor: null,
-                color: '#ffffff',
-                hoverOpacity: 0.7,
-                selectedColor: '#1de9b6',
-                enableZoom: true,
-                showTooltip: true,
-                values: sample_data,
-                scaleColors: [ '#1de9b6', '#03a9f5' ],
-                normalizeFunction: 'polynomial'
-            } );
-        } )( jQuery );
+  //           jQuery( '#vmap' ).vectorMap( {
+  //               map: 'world_en',
+  //               backgroundColor: null,
+  //               color: '#ffffff',
+  //               hoverOpacity: 0.7,
+  //               selectedColor: '#1de9b6',
+  //               enableZoom: true,
+  //               showTooltip: true,
+  //               values: sample_data,
+  //               scaleColors: [ '#1de9b6', '#03a9f5' ],
+  //               normalizeFunction: 'polynomial'
+  //           } );
+  //       } )( jQuery );
   function edit_stan(id){
     alert(id);
 }
